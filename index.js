ea341dfa2ec0af7fcdfdaa319a21ecbea5ee5245
Logan McNatt <logan.m.mcnatt@gmail.com>
2023-12-02 11:24:39 -0700

    Added aws funcionality

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+let app = express();
+const port = process.env.PORT || 3000;
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname));
+const isLocal = process.env.NODE_ENV === 'development';
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || "localhost",
+        user: process.env.RDS_USERNAME || "test",
+        password: process.env.RDS_PASSWORD || "SuperPass",
+        database: process.env.RDS_DB_NAME || "intex1_test",
+        port: process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, () => console.log("Website is running"));
