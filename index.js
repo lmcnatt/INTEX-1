23f01506c9d0b5ab1f5149d5fc67ad3f92a615a5
benmorey <dbmoreyjr@gmail.com>
2023-12-05 10:44:22 -0700

    more tests

--- index.js
+++ index.js
@@ -44,21 +44,25 @@ app.post("/loginsubmit", (req, res) => {
             for (i = 0; i < user.length; i++){
                 if(user[i].username == loginUsername){
                     if(user[i].password == loginPassword){
-                        unlocked = true;
-                        // console.log(user[i].password + " | " + loginUsername)
+                        unlocked = true;   
+                    }
+                else{
+                    console.log(user[i].password + " | " + loginUsername)
+                    console.log(user[i].password + " | " + loginUsername)
+                }
+                    
                         // console.log("Please correct your username")
                         // res.render("login")
                     }
                     // console.log(user[i].username + " | " + loginUsername)
                     // console.log("Please correct your username")
                     // res.render("login")
-                }
-                if(unlocked == true){
-                    res.render("adminDashboard")
-                }
-                else{
-                    res.render("login")
-                }
+            }
+            if(unlocked == true){
+                res.render("home")
+            }
+            else{
+                res.render("login")
             }
             
             
