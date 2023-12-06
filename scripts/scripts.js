6788d6dcce18061f60be55d755b580500f391fc0
benmorey <dbmoreyjr@gmail.com>
2023-12-06 16:30:57 -0700

    I'm back

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,17 +2,18 @@
 //Animation Observer
 const hiddenElements = document.querySelectorAll('.hidden');
 
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        entry.target.classList.toggle('show', entry.isIntersecting);
-        if (entry.isIntersecting){
-            observer.unobserve(entry.target)
-        }
+            entry.target.classList.toggle('show', entry.isIntersecting);
+            if (entry.isIntersecting){
+                observer.unobserve(entry.target)
+            }
     });
 },
 {
-    threshold: 0,
-    rootMargin: "0px"
+    threshold: 0.9,
+    rootMargin: "-100px"
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
